a75f67d32c6691681decb5a45aade46fb17a4598
Islaster <52678788+Islaster@users.noreply.github.com>
2022-01-07 11:48:45 -0800

    exercise 3

--- python_container.py
+++ python_container.py
@@ -8,3 +8,7 @@ print(students[len(students)-1])
 foods = ('pizza', 'apples', 'sushi', 'tacos', 'burgers', 'pasta')
 for food in foods:
     print(f'{food} is a good food')
+# Exercise 3
+last_two = slice(len(foods)-2, len(foods))
+print(foods[last_two])
+  
