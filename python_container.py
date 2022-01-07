5c6a249aea7e76b94bc338f47713b250a2493024
Islaster <52678788+Islaster@users.noreply.github.com>
2022-01-07 12:27:57 -0800

    exercise 4

--- python_container.py
+++ python_container.py
@@ -1,14 +1,22 @@
 # Exercise 1
-students = ['jack', 'john', 'jim', 'kim', 'kevin', 'kerry']
-print(students[1])
-print(students[len(students)-1])
+#students = ['jack', 'john', 'jim', 'kim', 'kevin', 'kerry']
+#print(students[1])
+#print(students[len(students)-1])
 
 
 # Exercise 2
-foods = ('pizza', 'apples', 'sushi', 'tacos', 'burgers', 'pasta')
-for food in foods:
-    print(f'{food} is a good food')
+#foods = ('pizza', 'apples', 'sushi', 'tacos', 'burgers', 'pasta')
+#for food in foods:
+#    print(f'{food} is a good food')
+
 # Exercise 3
-last_two = slice(len(foods)-2, len(foods))
-print(foods[last_two])
-  
+#last_two = slice(len(foods)-2, len(foods))
+#print(foods[last_two])
+
+#  Exercise 4
+home_town = {
+    'city': 'South Bend',
+    'state': 'Indiana',
+    'population': 102037 
+}
+print(f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
