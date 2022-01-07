a319024ba37ee7e235bbb3c991fce70092b6086f
Islaster <52678788+Islaster@users.noreply.github.com>
2022-01-07 12:39:27 -0800

    Exercise 6

--- python_container.py
+++ python_container.py
@@ -1,11 +1,11 @@
 # Exercise 1
-#students = ['jack', 'john', 'jim', 'kim', 'kevin', 'kerry']
+students = ['jack', 'john', 'jim', 'kim', 'kevin', 'kerry']
 #print(students[1])
 #print(students[len(students)-1])
 
 
 # Exercise 2
-#foods = ('pizza', 'apples', 'sushi', 'tacos', 'burgers', 'pasta')
+foods = ('pizza', 'apples', 'sushi', 'tacos', 'burgers', 'pasta')
 #for food in foods:
 #    print(f'{food} is a good food')
 
@@ -14,13 +14,24 @@
 #print(foods[last_two])
 
 #  Exercise 4
-home_town = {
-    'city': 'South Bend',
-    'state': 'Indiana',
-    'population': 102037 
-}
+#home_town = {
+#    'city': 'South Bend',
+#    'state': 'Indiana',
+#    'population': 102037 
+#}
 #print(f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
 
 # Exercise 5
-for key in home_town:
-    print(f'{key} = {home_town[key]}')
+#for key in home_town:
+#    print(f'{key} = {home_town[key]}')
+
+# Exercise 6
+cohort = []
+for student in students:
+    i = students.index(student)
+    cohort.append({
+        'student': student,
+        'fav_food': foods[i]
+    })
+for c in cohort:
+    print(c)
